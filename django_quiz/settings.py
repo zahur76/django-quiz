6935d483a1b0b104e04cfe7ee6645fe8544173a6
zahur <zahurmeerun@hotmail.com>
2021-11-03 20:42:56 +0400

    create staff app with urls and views

--- django_quiz/settings.py
+++ django_quiz/settings.py
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-        
+    'django.contrib.staticfiles',        
     'home',
     'login',
+    'staff',
 ]
 
 MIDDLEWARE = [
--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [    
     path('', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),            
+    path('logout', views.logout_view, name='logout'),
 ]
