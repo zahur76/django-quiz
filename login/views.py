54b766cb2ec55eb7c43ddd39f340b34641a4d16e
zahur <zahurmeerun@hotmail.com>
2021-11-03 19:59:43 +0400

    add login app with login and logout views

---
+++ login/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib import messages
+
+# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        ''' View to login as admin '''
+        username = request.POST['username']    
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login Successful!')
+            return redirect(reverse('home'))
+        else:
+            # Return an 'invalid login' error message
+            messages.error(request, 'Login Unsuccessful!')
+            return redirect(reverse('home'))
+
+def logout_view(request):
+    ''' View to logout as admin '''
+    logout(request)
+    messages.success(request, 'Logout Successful!')
+    return redirect(reverse('home'))
---
+++ static/js/base.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){    
+    $(".login-button").click(function(){
+        console.log('zahur')             
+        $("#loginModal").show();                          
+    });
+
+    $(".close").click(function(){        
+        $("#loginModal").hide();        
+    });
+
+    /* Function to clear flash messages after 3's*/  
+    setTimeout(function(){
+        $(".flash-message").hide("slow");
+    }, 3000 ); // 5 secs
+
+    /* Allow password to become visible*/ 
+    $("body").on('click', '.eye-icon', function() {
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        input = $("#password");
+        if (input.attr("type") === "password") {
+          input.attr("type", "text");
+        } else {
+          input.attr("type", "password");
+        }
+    });
+});
