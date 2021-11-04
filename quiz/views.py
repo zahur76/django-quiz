2897402edf73cd6e92826f8d1695089f263c14b8
zahur <zahurmeerun@hotmail.com>
2021-11-04 17:45:21 +0400

    add update quiz function

--- quiz/views.py
+++ quiz/views.py
@@ -52,4 +52,27 @@ def delete_quiz(request, quiz_id):
     quiz.delete()
     messages.success(request, 'Quiz record deleted!')
     return redirect(reverse('quiz'))
-    
+
+def update_quiz(request, quiz_id):
+    """View for quiz management"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Permision Denied!.')
+        return redirect(reverse('home'))
+    quiz = get_object_or_404(Quiz, id=quiz_id)          
+    if request.method == 'POST':        
+        form = add_quizForm(request.POST, request.FILES, instance=quiz)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Quiz updated!')
+            return redirect(reverse('quiz'))
+        else:
+            messages.error(
+                request, 'Quiz could not be updated. \
+                    Tru again.')
+            return redirect(reverse('quiz'))
+    form = add_quizForm(instance=quiz)
+    context = {
+        'form': form,
+        'quiz': quiz,
+        }
+    return render(request, 'quiz/update_quiz.html', context)
