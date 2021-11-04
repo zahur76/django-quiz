d187075497f4b07f12c5b3d67937f3160494f9f1
zahur <zahurmeerun@hotmail.com>
2021-11-04 15:08:24 +0400

    add post method for employee quiz details

--- home/views.py
+++ home/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
+from staff.models import Staff
 
 # Create your views here.
 def home(request):
     """ A view to return the home page """
+    if request.method == 'POST':
+        employee_num = int(request.POST['employee_num'])
+        try:
+            staff = Staff.objects.get(employee_number=employee_num)
+        except Staff.DoesNotExist:
+            messages.error(
+            request, 'Incorrect Employee Number!')
+            return render(request, 'home/index.html')
 
+        if staff:
+            context = {
+                'staff': staff,
+            }
+            return render(request, 'home/index.html', context)
+        
     return render(request, 'home/index.html')
